14a014d307ebb676cd8480935d11ce398b9e7555
Jonathan <jonathanxu25@gmail.com>
2021-03-18 23:52:44 +0800

    stream hls from mac works, working on render on canvas

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Hls from "hls.js";
 
 export default function VideoPlayer() {
   const videoRef = useRef(null);
+  const canvasRef = useRef(null)
 
   useEffect(() => {
     const hlsUrl = `https://dq86krv8mpwpa.cloudfront.net/f5cc4e0a-292e-4e5d-b838-2911cc154f18/hls/Laptop Repair.m3u8`;
@@ -10,10 +11,7 @@ export default function VideoPlayer() {
     if (video.canPlayType("application/vnd.apple.mpegurl")) {
       console.log("natively supported");
       // If HLS is natively supported, let the browser do the work!
-      video.src = "hlsUrl";
-      video.addEventListener("loadedmetadata", function () {
-        video.play();
-      });
+      video.src = hlsUrl;
     } else if (Hls.isSupported()) {
       console.log("not natively supported");
       // If the browser supports MSE, use hls.js to play the video
@@ -36,19 +34,44 @@ export default function VideoPlayer() {
       });
       hls.loadSource(hlsUrl);
       hls.attachMedia(video);
-      hls.on(Hls.Events.MANIFEST_PARSED, function () {
-        video.play();
-      });
     } else {
       alert("Please use a modern browser to play the video");
     }
+
+    video.addEventListener(
+      "play",
+      () => {
+        requestAnimationFrame(updateCanvas);
+      },
+      false
+    );
   }, []);
+
+  const updateCanvas = () => {
+    const video = videoRef.current
+    const canvasContext = canvasRef.current.getContext("2d")
+    if (video.ended || video.paused) {
+      return;
+    }
+
+    canvasContext.drawImage(
+      video,
+      0,
+      0,
+      video.width,
+      video.height
+    );
+
+    requestAnimationFrame(updateCanvas);
+  }
 
   return (
     <div>
-      <div>thing1</div>
-      <video ref={videoRef} />
-      <div>thing2</div>
+      <video ref={videoRef} controls width="600"
+        height="338"/>
+      <div>
+      <canvas ref={canvasRef} id="canvas" width="600" height="338"/>
+    </div>
     </div>
   );
 }
