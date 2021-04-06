30f2b8b2d5f8b727af4378db901c63a550b61478
Kunal Shah <me@kunal.sh>
2021-04-06 16:57:20 +0800

    video player controls and progress bar ▶️⏯

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -497,7 +497,6 @@ export default function VideoPlayer() {
           height={videoHeight}
           style={{ position: "absolute", left: 0, top: 0, zIndex: 0 }}
         />
-
         <MouseCanvas
           ref={secondayCanvas}
           width={videoWidth}
@@ -518,6 +517,132 @@ export default function VideoPlayer() {
         >
           {captionChars}
         </div>
+        <div>
+          <progress
+            id="progress-bar"
+            min="0"
+            max="100"
+            style={{
+              width: "100%",
+              position: "absolute",
+              display: "flex",
+              justifyContent: "center",
+              bottom: 26,
+              zIndex: 4,
+              left: 0,
+              height: 10,
+              width: videoWidth,
+            }}
+            value={videoProgress}
+            onClick={(e) => {
+              const currentTargetRect = e.currentTarget.getBoundingClientRect();
+              const left = e.pageX - currentTargetRect.left;
+              const percentage = left / 200;
+              const vidTime = videoRef.current.duration * percentage;
+              videoRef.current.currentTime = vidTime;
+              setPlaying(true);
+            }}
+          >
+            {videoProgress}% played
+          </progress>
+        </div>
+        <div
+          style={{
+            position: "absolute",
+            display: "flex",
+            justifyContent: "center",
+            bottom: 0,
+            left: 0,
+            width: videoWidth,
+            height: 20,
+            paddingTop: 5,
+            paddingBottom: 5,
+            zIndex: 3,
+            backgroundColor: "#eee",
+          }}
+        >
+          <div
+            onClick={() => {
+              // videoRef.current.setVideoProgress(videoProgress - 10);
+            }}
+            style={{ width: 20 }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0019 16V8a1 1 0 00-1.6-.8l-5.333 4zM4.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0011 16V8a1 1 0 00-1.6-.8l-5.334 4z"
+              />
+            </svg>
+          </div>
+          <div
+            onClick={() => {
+              playing ? videoRef.current.pause() : videoRef.current.play();
+              setPlaying(!playing);
+            }}
+            style={{ width: 20 }}
+          >
+            {playing ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            )}
+          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M11.933 12.8a1 1 0 000-1.6L6.6 7.2A1 1 0 005 8v8a1 1 0 001.6.8l5.333-4zM19.933 12.8a1 1 0 000-1.6l-5.333-4A1 1 0 0013 8v8a1 1 0 001.6.8l5.333-4z"
+            />
+          </svg>
+        </div>
+        {/* // toggle "showControls" state */}
       </div>
     </div>
   );
