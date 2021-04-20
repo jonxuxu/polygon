59c92d70f3052e2d60bf7ed0fd8886171733b527
Kunal Shah <me@kunal.sh>
2021-04-20 13:07:13 +0800

    specify file type

--- frontend/pages/uploads.tsx
+++ frontend/pages/uploads.tsx
@@ -41,7 +41,7 @@ const App = () => {
               >
                 <path
                   d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                  stroke-width="2"
+                  strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                 />
@@ -55,11 +55,12 @@ const App = () => {
                     type="file"
                     className="sr-only"
                     onChange={uploadPhoto}
+                    accept="video/*"
                   />
                 </label>
                 <p className="pl-1">or drag and drop</p>
               </div>
-              <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+              <p className="text-xs text-gray-500">MP4, MOV up to 10MB</p>
             </div>
           </div>
         </div>
