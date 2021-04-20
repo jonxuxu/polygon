5e6cf11a222293fd32990bf9ace1532e3ad70fe2
Kunal Shah <me@kunal.sh>
2021-04-20 13:23:10 +0800

    query video by id

--- frontend/pages/api/video/[id].ts
+++ frontend/pages/api/video/[id].ts
@@ -2,7 +2,7 @@ import prisma from "../../../prisma/client";
 
 export default async (req, res) => {
   const video = await prisma.videos.findUnique({
-    where: { id: req.body.id },
+    where: { id: req.query.id },
   });
 
   return res.json(video);
