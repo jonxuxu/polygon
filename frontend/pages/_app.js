d3c28987ea94a52368ef5b2e432b4788c8f0ef84
Kunal Shah <me@kunal.sh>
2021-04-26 18:14:39 +0800

    analytics

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,9 @@
 import "../globals.css";
 import { Provider } from "next-auth/client";
+import { useAnalytics } from "components/Analytics";
 
 function MyApp({ Component, pageProps }) {
+  useAnalytics();
   return (
     <Provider session={pageProps.session}>
       <Component {...pageProps} />
