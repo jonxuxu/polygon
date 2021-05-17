fe9088ad4ee633f38a5be2280c4b469cd5076cc4
Kunal Shah <me@kunal.sh>
2021-05-17 14:01:17 +0800

    update slack hook

--- frontend/pages/api/video/update.ts
+++ frontend/pages/api/video/update.ts
@@ -1,13 +1,14 @@
+import { sendLog } from "utils/sendLog";
 import prisma from "../../../prisma/client";
 
 export default async (req, res) => {
   const { id, ...rest } = req.body;
   if (!id) return res.json({ error: "ERROR: must provide id to update video" });
-  console.log(`updating video ${id}`, rest);
   const updatedVideo = await prisma.videos.update({
     data: rest,
     where: { id: req.body.id },
   });
+  sendLog(`updated video ${updatedVideo.cuid}`);
 
   return res.json(updatedVideo);
 };
--- frontend/utils/sendLog.tsx
+++ frontend/utils/sendLog.tsx
@@ -1,10 +1,7 @@
-const USER_BEHAVIOR_URL =
-  "https://hooks.slack.com/services/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";
-
 export const sendLog = async (text: string) => {
   console.log(text);
 
-  return await fetch(USER_BEHAVIOR_URL, {
+  return await fetch(process.env.SLACK_HOOK_URL, {
     method: "POST",
     body: JSON.stringify({
       text,
