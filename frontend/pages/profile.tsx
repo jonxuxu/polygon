01733b99eca9eeb27d2924c8221af69d59edc121
Kunal Shah <me@kunal.sh>
2021-05-18 17:15:14 +0800

    no indigo not today sir

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -120,7 +120,7 @@ function ProfileTabs({ tab, setTab }) {
         <select
           id="tabs"
           name="tabs"
-          className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+          className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm rounded-md"
           defaultValue={tab}
           onChange={(e) => setTab(e.target.value)}
         >
