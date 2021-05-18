099000a99384c5a22d5007dba20533fafcf454d4
Kunal Shah <me@kunal.sh>
2021-05-18 15:46:34 +0800

    share and save buttons

---
+++ frontend/pages/video/[cuid]/SaveButton.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { mutate } from "swr";
+import { fetcher, useMe, useVideo } from "utils/fetcher";
+
+export function SaveButton() {
+  const router = useRouter();
+  const { video } = useVideo({ cuid: router.query?.cuid });
+  const { me } = useMe();
+  if (!me || !video) return null;
+  return (
+    <button
+      className="save"
+      onClick={async () => {
+        const savedBy = video.savedBy.find((u) => u.id === me.id)
+          ? { disconnect: { email: me.email } }
+          : { connect: { email: me.email } };
+        await fetcher("/api/video/update", {
+          id: video.id,
+          savedBy,
+        });
+        await mutate("/api/video/" + video.cuid);
+      }}
+    >
+      <img
+        src="/add-list.svg"
+        alt="save"
+        style={{
+          width: 20,
+          height: 20,
+          cursor: "pointer",
+          marginRight: 8,
+        }}
+      />
+      <span className="text-gray-500">
+        {video.savedBy.find((u) => u.id === me.id) ? "Unsave" : "Save"}
+      </span>
+    </button>
+  );
+}
---
+++ frontend/pages/video/[cuid]/ShareButton.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import { useMe, useVideo } from "utils/fetcher";
+import { copyToClipboard } from "utils/text";
+
+export function ShareButton() {
+  const router = useRouter();
+  const { video } = useVideo({ cuid: router.query?.cuid });
+  const [copied, setCopied] = useState(false);
+  const { me } = useMe();
+  if (!me || !video) return null;
+  return (
+    <button
+      className="save"
+      onClick={() => {
+        copyToClipboard(window.location.href);
+        setCopied(true);
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-4 w-4 text-gray-700 mr-2 mt-0.5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
+        />
+      </svg>
+      <span className="text-gray-500">{copied ? "Copied!" : "Share Link"}</span>
+    </button>
+  );
+}
